847f9eac7c4f448de6b59a95fcf6f4596d4c2392
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2023-01-24 15:55:08 +0900

    기본 세팅

---
+++ Survival/com/Jongyeol/NameServer/Survival/Money/Survival/MainPlugin.java
@@ -0,0 +1,15 @@
+package com.Jongyeol.NameServer.Survival.Money.Survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
